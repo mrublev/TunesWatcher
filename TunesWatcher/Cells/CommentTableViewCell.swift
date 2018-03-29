4a3e2edafe27ce8d18a1e1d3276795ef4479cc42
Michael Rublev <michael.rublev@loudclear.com.au>
2018-03-29 17:59:49 +0600

    Get and display first page of comments

---
+++ TunesWatcher/Cells/CommentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentTableViewCell.swift
+//  TunesWatcher
+//
+//  Created by Michael Rublev on 29/03/2018.
+//  Copyright © 2018 Michael Rublev. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TunesWatcher/Controllers/CommentsViewController.swift
+++ TunesWatcher/Controllers/CommentsViewController.swift
@@ -8,37 +8,92 @@
 
 import UIKit
 import RealmSwift
+import Alamofire
 
-class CommentsViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+class CommentsViewController: UITableViewController {
+    
+    let URLTemplate = "https://itunes.apple.com/ru/rss/customerreviews/id=<APP_ID>/sortBy=mostRecent/json"
+    var requestURL: URL?
+    var objects: [FeedComment]?
     
     var appIdEntry: AppIdEntry? {
         didSet {
-            configureView()
-        }
-    }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let appEntry = appIdEntry {
-            if let label = detailDescriptionLabel {
-                label.text = appEntry.applicationId
+            if let id = appIdEntry?.applicationId {
+                let requestString: String = URLTemplate.replacingOccurrences(of: "<APP_ID>", with: id)
+                requestURL = URL.init(string: requestString)
             }
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let url = requestURL {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            
+            Alamofire.request(url).responseJSON { response in
+                if let json = response.result.value {
+                    self.processJson(json as! [String: Any])
+                }
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                self.tableView.reloadData()
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func processJson(_ feed: [String: Any]) {
+        if let feedUnwrap = feed["feed"] as? [String: Any] {
+            if let commentEntries = feedUnwrap["entry"] as? [[String: Any]] {
+                var allComments = [FeedComment]()
+                
+                commentEntries.forEach({ (entry: [String: Any]) in
+                    if entry["author"] != nil {
+                        allComments.append(commentForEntry(entry))
+                    }
+                })
+                objects = allComments
+            }
+        }
     }
+    
+    func commentForEntry(_ entry: [String: Any]) -> FeedComment {
+        let comment = FeedComment()
+        if let authorDict = entry["author"] as? [String: Any], let nameDict = authorDict["name"] as? [String: Any], let label = nameDict["label"] as? String  {
+            comment.authorName = label
+        }
+        if let ratingDict = entry["im:rating"] as? [String: Any], let label = ratingDict["label"] as? String {
+            comment.rating = label
+        }
+        if let titleDict = entry["title"] as? [String: Any], let label = titleDict["label"] as? String {
+            comment.title = label
+        }
+        if let contentDict = entry["content"] as? [String: Any], let label = contentDict["label"] as? String {
+            comment.content = label
+        }
+        return comment
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects == nil ? 0 : objects!.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! CommentTableViewCell
+        let object = objects![indexPath.row]
+        
+        cell.ratingLabel.text = object.rating
+        cell.titleLabel.text = object.title
+        cell.descriptionLabel.text = object.content
 
-
+        return cell
+    }
 }
 
